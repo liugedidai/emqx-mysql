809428d4af3ede23889d8a3e9407d3625d2b1cf6
liugedidai <yangliu5830@gmail.com>
2020-06-19 13:44:23 +0800

    Update emqx_mysql.erl

--- src/emqx_mysql.erl
+++ src/emqx_mysql.erl
@@ -17,13 +17,13 @@
 
 
 load_hook(Env) ->
-	emqx:hook('client.connected', fun ?MODULE, on_client_connected/3, [Env]).
-    	emqx:hook('client.disconnected', fun ?MODULE, on_client_disconnected/4, [Env]).
+	emqx:hook('client.connected', fun ?MODULE:on_client_connected/3, [Env]).
+    	emqx:hook('client.disconnected', fun ?MODULE:on_client_disconnected/4, [Env]).
 	emqx:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]).
 
 unload_hook() ->
-	emqx:unhook('client.connected', fun ?MODULE, on_client_connected/3).
-    	emqx:unhook('client.disconnected', fun ?MODULE, on_client_disconnected/4).
+	emqx:unhook('client.connected', fun ?MODULE:on_client_connected/3).
+    	emqx:unhook('client.disconnected', fun ?MODULE:on_client_disconnected/4).
 	emqx:unhook('message.publish', fun ?MODULE:on_message_publish/2).
 
 on_client_connected(ClientInfo = #{clientid := ClientId, peerhost := Peerhost}, ConnInfo, _Env) ->
