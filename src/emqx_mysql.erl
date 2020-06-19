ab295d4a809c00b901061f438003f1ee20246fb8
liugedidai <yangliu5830@gmail.com>
2020-06-19 14:40:56 +0800

    Update emqx_mysql.erl

--- src/emqx_mysql.erl
+++ src/emqx_mysql.erl
@@ -13,25 +13,27 @@
 -define(CLIENT_DISCONNECTED_SQL,
                  <<"update mqtt_client set state = ?, offline_at "
                    "= now() where clientid = ?">>).
--export([load_hook/1, unload_hook/0, on_message_publish/2,on_client_connected/3,on_client_disconnected/4]).
+-export([load_hook/1, unload_hook/0, on_message_publish/2, on_client_connected/3, on_client_disconnected/4]).
 
 
 load_hook(Env) ->
-	emqx:hook('client.connected', fun ?MODULE:on_client_connected/3, [Env]),
-    	emqx:hook('client.disconnected',fun ?MODULE:on_client_disconnected/4, [Env]),
+	emqx:hook('client.connected',    {?MODULE, on_client_connected, [Env]}),
+    	emqx:hook('client.disconnected', {?MODULE, on_client_disconnected, [Env]}),
 	emqx:hook('message.publish', fun ?MODULE:on_message_publish/2, [Env]).
 
 unload_hook() ->
-	emqx:hook('client.connected', fun ?MODULE:on_client_connected/3),
-    	emqx:hook('client.disconnected', fun ?MODULE:on_client_disconnected/4),
+	emqx:unhook('client.connected',    {?MODULE, on_client_connected}),
+    	emqx:unhook('client.disconnected', {?MODULE, on_client_disconnected}),
 	emqx:unhook('message.publish', fun ?MODULE:on_message_publish/2).
 
 on_client_connected(ClientInfo = #{clientid := ClientId, peerhost := Peerhost}, ConnInfo, _Env) ->
     emqx_mysql_cli:query(?CLIENT_CONNECTED_SQL, [binary_to_list(ClientId),null,tuple_to_list(Peerhost),null]),
+    %%io:format("Client(~s) connected, ClientInfo:~n~p~n, ConnInfo:~n~p~n, Peerhost:~n~p~n", [ClientId, ClientInfo, ConnInfo, Peerhost]),
     ok.
 
 on_client_disconnected(ClientInfo = #{clientid := ClientId}, ReasonCode, ConnInfo, _Env) ->
     emqx_mysql_cli:query(?CLIENT_DISCONNECTED_SQL, [null,binary_to_list(ClientId)]),
+    %%io:format("Client(~s) disconnected due to ~p, ClientInfo:~n~p~n, ConnInfo:~n~p~n",[ClientId, ReasonCode, ClientInfo, ConnInfo]),
     ok.
 
 
